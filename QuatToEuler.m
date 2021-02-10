1bb452e25f3141f7313575819b54ca5f1f91d530
Jérémie Bannwarth <jban039@aucklanduni.ac.nz>
2021-02-10 17:43:01 +1300

    PHD-155 Fix bad indent

--- QuatToEuler.m
+++ QuatToEuler.m
@@ -11,28 +11,28 @@ function [ varargout ] = QuatToEuler( quat )
 %           - pitch: Mx1 vector of pitch angles
 %           - yaw:   Mx1 vector of yaw angles
 %   Written by: J.X.J. Bannwarth, 07/08/2017
-    
+
     nout = max(nargout,1);
     if ~( ( nout == 1 ) || ( nout == 3 ) )
         error( 'Invalid number of output: only 1 and 3 output are supported' )
     end
-        
+
     [m, n] = size( quat );
-    
+
     if ( (m == 4) && (n == 1) )
         quat = quat';
     end
 
     eul(:,1) = atan2(2.0 .* (quat(:,1) .* quat(:,2) + quat(:,3) .* quat(:,4)), ...
-            1.0 - 2.0 .* (quat(:,2) .* quat(:,2) + quat(:,3) .* quat(:,3)));
-	eul(:,2) = asin(2.0 .* (quat(:,1) .* quat(:,3) - quat(:,4) .* quat(:,2)));
-	eul(:,3) = atan2(2.0 .* (quat(:,1) .* quat(:,4) + quat(:,2) .* quat(:,3)), ...
-            1.0 - 2.0 .* (quat(:,3) .* quat(:,3) + quat(:,4) .* quat(:,4)));
+        1.0 - 2.0 .* (quat(:,2) .* quat(:,2) + quat(:,3) .* quat(:,3)));
+    eul(:,2) = asin(2.0 .* (quat(:,1) .* quat(:,3) - quat(:,4) .* quat(:,2)));
+    eul(:,3) = atan2(2.0 .* (quat(:,1) .* quat(:,4) + quat(:,2) .* quat(:,3)), ...
+        1.0 - 2.0 .* (quat(:,3) .* quat(:,3) + quat(:,4) .* quat(:,4)));
 
     if ( (m == 4) && (n == 1) )
         eul = eul';
     end
-    
+
     if ( nout == 3 )
         varargout{1} = eul(:,1);
         varargout{2} = eul(:,2);
@@ -40,5 +40,5 @@ function [ varargout ] = QuatToEuler( quat )
     else
         varargout{1} = eul;
     end
-    
+
 end
