ddf71212fc255e34a8ba9630030b70ecadc1cd4a
Jérémie Bannwarth <jban039@aucklanduni.ac.nz>
2021-02-10 15:14:46 +1300

    PHD-155 Finish function to add lines denoting mode changes
    Convert to function, clean up and comment

--- AddModeLabels.m
+++ AddModeLabels.m
@@ -1,52 +1,101 @@
+function AddModeLabels( modes, inverted )
 %ADDMODELABELS Add mode transition labels to plot
+%   Input:
+%       - modes: timetable of mode changes
+%   Shows modes in the following order of precedence:
+%       - OFFB > POS > VEL > ALT > ATT > RATES
+%   Draws a colored line along with a label for each mode change. Lines are
+%   much easier to handle than shaded areas.
 %   Written: 2021/02/09, J.X.J. Bannwarth
-
-% Add modes
-xLims = xlim();
-yLims = ylim();
-
-labels = { 'OFFB'; 'POS'; 'ALT'; 'MANUAL' };
-fields = { 'flag_control_offboard_enabled';
-           'flag_control_position_enabled';
-           'flag_control_altitude_enabled';
-           'flag_control_manual_enabled' };
-colors = { 'r', 'g', 'b', 'k' };
-labelLocs = zeros( size( mode.(fields{1}) ) );
-
-for ii = 1:length(fields)
-    modeOnIdx = find( diff(mode.(fields{ii})) == 1 );
-    modeOffIdx = find( diff(mode.(fields{ii})) == -1 );
-    
-    for jj = 1:length( modeOnIdx )
-        if labelLocs( modeOnIdx(jj) ) ~= 1
-            startLoc =  mode.timestamp(modeOnIdx(jj));
-            plot( [startLoc startLoc], ...
-                [yLims(1), yLims(2)], '--', 'color', colors{ii} )
-            labelLocs( modeOnIdx(jj) ) = 1;
-            h = text( startLoc+0.02*diff(xLims), ...
-                yLims(1) + 0.05*diff(yLims), labels{ii}, 'FontSize', 8, ...
-                'Color', colors{ii}, 'Rotation', 90 );
-        end
+    %% Arguments
+    arguments
+        modes timetable
+        inverted (1,1) logical = false
     end
+    %% Set-up
+    % Recognised mode labels, corresponding fields, and color codes
+    labels = { 'OFFB'; 'POS'; 'VEL'; 'ALT'; 'ATT'; 'RATES' };
+    fields = { 'flag_control_offboard_enabled';
+               'flag_control_position_enabled';
+               'flag_control_velocity_enabled';
+               'flag_control_altitude_enabled';
+               'flag_control_attitude_enabled';
+               'flag_control_rates_enabled'  };
+    colors = parula( length(fields) );
+    colors = mat2cell( colors, ones(length(fields),1), 3 );
     
-    for jj = 1:length( modeOffIdx )
-        for kk = ii+1:length(fields)
-            if (labelLocs( modeOffIdx(jj) ) ~= 1) && ...
-                    ( mode.(fields{kk})( modeOffIdx(jj) ) == 1 )
-                startLoc =  mode.timestamp(modeOffIdx(jj));
-                plot( [startLoc startLoc], ...
-                    [yLims(1), yLims(2)], '--', 'color', colors{kk} )
-                labelLocs( modeOffIdx(jj) ) = 1;
-                h = text( startLoc+0.02*diff(xLims), ...
-                    yLims(1) + 0.05*diff(yLims), labels{kk}, 'FontSize', 8, ...
-                    'Color', colors{kk}, 'Rotation', 90 );
-                break
+    %% Add labels
+    % Array of line locations to avoid drawing two lines on top of one another
+    lineLocs = zeros( size( modes.(fields{1}) ) );
+
+    % Start from the mode of highest precedence and work way down
+    for ii = 1:length(fields)
+        % Find when the current mode is activated and deactivated
+        modeOnIdx = find( diff(modes.(fields{ii})) == 1 ) + 1;
+        modeOffIdx = find( diff(modes.(fields{ii})) == -1 ) + 1;
+
+        % Handle the mode being on on the first sample
+        if modes.(fields{ii})(1) == 1
+            modeOnIdx = [1 modeOnIdx];
+        end
+
+        % Draw lines when mode is activated
+        for jj = 1:length( modeOnIdx )
+            if lineLocs( modeOnIdx(jj) ) ~= 1
+                DrawTransitionLine( modes.timestamp(modeOnIdx(jj)), ...
+                    colors{ii}, labels{ii}, inverted );
+                lineLocs( modeOnIdx(jj) ) = 1;
+            end
+        end
+
+        % Draw lines when mode is deactivated
+        % Note: Lower level modes are not necessarily deactivated when
+        % activating a higher level mode. For example, POSCTL is still active
+        % while in OFFBOARD. Therefore, we cannot rely on rising edges of lower
+        % level modes to accurately determine the current mode
+        for jj = 1:length( modeOffIdx )
+            % Cycle through lower level mode to find which one is still active
+            for kk = (ii+1):length(fields)
+                if (lineLocs( modeOffIdx(jj) ) ~= 1) && ...
+                        ( modes.(fields{kk})( modeOffIdx(jj) ) == 1 )
+                    DrawTransitionLine( modes.timestamp(modeOffIdx(jj)), ...
+                        colors{kk}, labels{kk}, inverted );
+                    lineLocs( modeOffIdx(jj) ) = 1;
+                    break
+                end
             end
         end
     end
 end
 
-nLines = sum( labelLocs );
-lh = gco;
-s = get( lh, 'string' );
-set( lh, 'string', s(1:end-nLines) )
+%% Helper function
+function DrawTransitionLine( xLoc, color, label, inverted )
+%DRAWTRANSITIONLINE Draw mode transition line with label
+%   Inputs:
+%       - xLoc:  location on x-axis to put the mode transition line
+%       - color: color code or 3x1 array of normalized RGB values
+%       - label: label to put to the right of the mode transition line
+%   Written: 2021/02/10, J.X.J. Bannwarth
+    arguments
+        xLoc
+        color
+        label (1,:) char
+        inverted (1,1) logical = false
+    end
+
+    % Axes limits
+    xLims = xlim();
+    yLims = ylim();
+    
+    % Line
+    plot( [xLoc xLoc], [yLims(1), yLims(2)], '-.', 'color', color )
+    
+    % Label
+    if inverted
+        text( xLoc+0.02*diff(xLims), yLims(2) - 0.05*diff(yLims), ...
+            label, 'FontSize', 8, 'Color', color, 'Rotation', 90 );
+    else
+        text( xLoc+0.02*diff(xLims), yLims(1) + 0.05*diff(yLims), ...
+            label, 'FontSize', 8, 'Color', color, 'Rotation', 90 );
+    end
+end
