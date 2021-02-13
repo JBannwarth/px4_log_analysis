3d03b40536540c17a6d4e1609a2ff063bcf69ba8
JBannwarth <jban039@aucklanduni.ac.nz>
2021-02-13 16:51:45 +1300

    PHD-155 If file is not found, search for it in dirIn + function description

--- LoadLog.m
+++ LoadLog.m
@@ -1,13 +1,43 @@
 function [flog, ulog] = LoadLog( fileIn, dirIn )
 %LOADLOG Load ulog file to structure of timetables
+%   [FLOG, ULOG] = LOADLOG( FILEIN ) loads FILEIN in the root folder 'logs'
+%   [FLOG, ULOG] = LOADLOG( FILEIN, DIRIN ) specifies the root folder DIRIN
+%
+%   Inputs:
+%       - fileIn: Name of the file to load. If the file name does not
+%                 contain the subfolder location, LoadLog will search all
+%                 subfolders in dirIn for the file
+%       - dirIn:  Name of the root folder containing all the logs
+%   Outputs:
+%       - flog:   Structure of timetables containing logged messages
+%       - ulog:   ulogreader object of the input flight log
+%
+%   See also LOADLATESTLOG, ULOGREADER.
+%
 %   Written: 2021/02/07, J.X.J. Bannwarth
-    %% Arguments
     arguments
         fileIn (1,:) char
         dirIn  (1,:) char = 'logs'
     end
     
     %% Load data
+    % If the file cannot be found, check if there is a matching file in one
+    % of the subfolders
+    if ~isfile( fullfile( dirIn, fileIn ) )
+        logs = dir( fullfile( dirIn, '**', '*.ulg' ) );
+        logIdx = find( strcmp( {logs.name}, fileIn ), 1 );
+        
+        if isempty( logIdx )
+            error( 'Input file ''%s'' not found', fileIn )
+        else
+            fprintf( 'Input file ''%s'' found in ''%s''\n', ...
+                fileIn, logs(logIdx).folder )
+            fileIn = logs(logIdx).name;
+            dirIn  = logs(logIdx).folder;
+        end
+    end
+    
+    % Load file
     ulog = ulogreader( fullfile( dirIn, fileIn ) );
 
     %% Extract relevant data to structure
