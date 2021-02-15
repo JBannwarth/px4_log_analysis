aefffb639bb6e06f38eea66e40a8e703d3ce004f
Jérémie Bannwarth <jban039@aucklanduni.ac.nz>
2021-02-15 17:44:51 +1300

    PHD-155 Do not load flight log if one is already loaded

--- ComparePositionSensors.m
+++ ComparePositionSensors.m
@@ -10,34 +10,48 @@
 %   Written: 2021/02/07, J.X.J. Bannwarth
 
 %% Set-up
-clearvars;
 clc;
 close all;
-flog = LoadLatestLog();
+clearvars -except flog ulog;
+
+% Only read-in data if it has not already been loaded
+if ~exist( 'flog', 'var' )
+    [flog, ulog] = LoadLatestLog();
+end
+
 timeFormat = 'mm:ss.SS';
+xAxisLabel = 'Time from boot (mm:ss)';
 
 %% Get data
 mocap = flog.vehicle_vision_position;
 lidar = flog.distance_sensor;
 flow  = flog.optical_flow;
 ekf2  = flog.vehicle_local_position;
+mode  = flog.vehicle_control_mode;
 
-%% Plot data
-% 2-D
+%% Plot 2-D data
 figure( 'name', '2-D trajectories' )
 % North
 subplot( 3, 1, 1 ); hold on; box on; grid on;
 plot( ekf2.timestamp, ekf2.x )
 plot( mocap.timestamp, mocap.x )
+
+% Formatting
 ylabel( 'North, x (m)' )
 xtickformat( timeFormat )
+axis tight
+AddModeLabels( mode )
 
 % East
 subplot( 3, 1, 2 ); hold on; box on; grid on;
 plot( ekf2.timestamp, ekf2.y )
 plot( mocap.timestamp, mocap.y )
+
+% Formatting
 ylabel( 'East, y (m)' )
 xtickformat( timeFormat )
+axis tight
+AddModeLabels( mode )
 
 % Down
 subplot( 3, 1, 3 ); hold on; box on; grid on;
@@ -45,14 +59,19 @@ plot( ekf2.timestamp, ekf2.z )
 plot( mocap.timestamp, mocap.z )
 plot( flow.timestamp, -flow.ground_distance_m )
 plot( lidar.timestamp, -lidar.current_distance )
+
+% Formatting
 set ( gca, 'ydir', 'reverse' )
 ylim( [-2, 0] )
-xlabel( 'Time from boot (mm:ss)' )
+xlabel( xAxisLabel )
 ylabel( 'Down, z (m)' )
 xtickformat( timeFormat )
-legend( {'EKF2', 'Mocap', 'PX4Flow', 'LIDAR'}, 'location', 'best' )
+legend( {'EKF2', 'Mocap', 'PX4Flow', 'LIDAR'}, 'location', 'best', ...
+    'AutoUpdate', 'off' )
+axis tight
+AddModeLabels( mode, true )
 
-% 3-D
+%% Plot 3-D data
 figure( 'name', '3-D trajectory' )
 plot3( ekf2.x, ekf2.y, ekf2.z )
 hold on; grid on; box on
