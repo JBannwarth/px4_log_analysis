543ea23b44cedd323ff3d5c61f3cf2e9dbc51b50
Jeremie Bannwarth <jban039@aucklanduni.ac.nz>
2021-03-18 13:18:05 +1300

    PHD-159 Add computation of PWM metrics

--- CalculateHoverMetrics.m
+++ CalculateHoverMetrics.m
@@ -39,27 +39,43 @@ function metrics = CalculateHoverMetrics( flogs )
     
     %% Initialize output table
     % Table column properties
-    varNames = { 'group', 'identifier', 'avgPosErr', 'avgPosErrNorm', ...
-        'rmsPosErr', 'rmsPosErrNorm', 'maxPosErr', 'maxPosNorm', 'avgAtt', ...
-        'rmsAttErr', 'rmsqDist', 'maxqDist', 'maxAttErr', 'fileName' }';
-    varTypes = { 'categorical', 'categorical', 'double', 'double', ...
-        'double', 'double', 'double', 'double', 'double', ...
-        'double', 'double', 'double', 'double', 'string' }';
-    varUnits = { '', '', 'm', 'm', 'm', 'm', 'm', 'm', 'deg', 'deg', ...
-        'deg', 'deg', 'deg', '' };
+    varDefs = { ...
+        'identifier'   , 'string'     , ''   ;
+        'group'        , 'categorical', ''   ;
+        'xLabel'       , 'categorical', ''   ;
+        'avgPosErr'    , 'double'     , 'm'  ;
+        'avgPosErrNorm', 'double'     , 'm'  ;
+        'rmsPosErr'    , 'double'     , 'm'  ;
+        'rmsPosErrNorm', 'double'     , 'm'  ;
+        'maxPosErr'    , 'double'     , 'm'  ;
+        'maxPosErrNorm', 'double'     , 'm'  ;
+        'avgAtt'       , 'double'     , 'deg';
+        'rmsAttErr'    , 'double'     , 'deg';
+        'rmsqDist'     , 'double'     , 'deg';
+        'maxqDist'     , 'double'     , 'deg';
+        'maxAttErr'    , 'double'     , 'deg';
+        'avgPwm'       , 'double'     , 'us' ;
+        'rmsPwm'       , 'double'     , 'us' ;
+        'maxPwm'       , 'double'     , 'us' ;
+        'minPwm'       , 'double'     , 'us' ;
+        'fileName'     , 'string'     , ''   ;
+        };
     
     % Create table
-    metrics = table( 'Size', [nLogs 14], 'VariableNames', varNames, ...
-        'VariableTypes', varTypes );
-    metrics.Properties.VariableUnits = varUnits;
+    metrics = table( 'Size', [nLogs size(varDefs, 1)], ...
+        'VariableNames', varDefs(:,1), 'VariableTypes', varDefs(:,2) );
+    metrics.Properties.VariableUnits = varDefs(:,3);
     
     % Cannot set dimensions right away, so go through and change column
     % dimensions
-    isTriplet = ~contains( varNames, {'Norm', 'qDist'} ) & ...
-        strcmp( varTypes, 'double' ) ;
-    for ii = 1:length( varNames )
+    isTriplet = ~contains( varDefs(:,1), {'Norm', 'qDist', 'Pwm'} ) & ...
+        strcmp( varDefs(:,2), 'double' ) ;
+    isOctuplet = contains( varDefs(:,1), 'Pwm' );
+    for ii = 1:length( varDefs(:,1) )
         if isTriplet(ii)
-            metrics.(varNames{ii}) = zeros( nLogs, 3 );
+            metrics.(varDefs{ii,1}) = zeros( nLogs, 3 );
+        elseif isOctuplet(ii)
+            metrics.(varDefs{ii,1}) = zeros( nLogs, 8 );
         end
     end
     
@@ -67,11 +83,12 @@ function metrics = CalculateHoverMetrics( flogs )
     idx = 1;
     for ii = 1:length( flogs )
         for jj = 1:length( flogs{ii} )
-             % Get important data
+            % Get important data
             pos   = flogs{ii}{jj}.vehicle_local_position;
             posSp = flogs{ii}{jj}.vehicle_local_position_setpoint;
             att   = flogs{ii}{jj}.vehicle_attitude;
             attSp = flogs{ii}{jj}.vehicle_attitude_setpoint;
+            act   = flogs{ii}{jj}.actuator_outputs;
 
             % Calculate position error
             posErr = [posSp.x posSp.y posSp.z] - [pos.x pos.y pos.z];
@@ -87,26 +104,45 @@ function metrics = CalculateHoverMetrics( flogs )
             avgAtt = eulerd( meanrot(q), 'ZYX', 'frame' );
             avgAtt = avgAtt(:,[3 2 1]); % Switch to [roll pitch yaw]
             
+            % Calculate actuator usage
+            pwm = act.output(:,1:act.noutputs(1));
+            
+            
             % Fill table
+            % Sorting columns
             if isIndividual
                 metrics.group(idx)     = 'NA';
+                metrics.xLabel(idx)    = 'NA';
                 metrics.identifier(idx)= 'NA';
             else
                 metrics.group(idx)     = flogs{ii}{jj}.group;
-                metrics.identifier(idx)= flogs{ii}{jj}.identifier(1:end-3);
+                metrics.xLabel(idx)    = flogs{ii}{jj}.identifier(1:end-3);
+                metrics.identifier(idx)= flogs{ii}{jj}.identifier;
             end
+            
+            % Position columns
             metrics.avgPosErr(idx,:)   = mean( posErr, 1 );
             metrics.avgPosErrNorm(idx) = mean( posErrNorm );
             metrics.rmsPosErr(idx,:)   = rms( posErr, 1 );
             metrics.rmsPosErrNorm(idx) = rms( posErrNorm );
             metrics.maxPosErr(idx,:)   = max( abs(posErr), [], 1 );
             metrics.maxPosErrNorm(idx) = max( posErrNorm );
+            
+            % Attitude columns
             metrics.avgAtt(idx,:)      = avgAtt;
             metrics.rmsAttErr(idx,:)   = rms( attErr, 1 );
             metrics.maxqDist(idx)      = rad2deg( max( qDist ) );
             metrics.maxAttErr(idx,:)   = max( abs(attErr), [], 1 );
             metrics.rmsqDist(idx)      = rad2deg( rms( qDist ) );
-            metrics.fileName(idx)      = flogs{ii}{jj}.filename;
+            
+            % Actuator usage columns
+            metrics.avgPwm(idx,:)      = mean( pwm, 1 );
+            metrics.rmsPwm(idx,:)      = rms( pwm - mean( pwm, 1 ), 1 );
+            metrics.minPwm(idx,:)      = min( pwm, [], 1 );
+            metrics.maxPwm(idx,:)      = max( pwm, [], 1 );
+            
+            % Extra columns
+            metrics.fileName(idx)      = [flogs{ii}{jj}.filename '.ulg'];
             
             idx = idx+1;
         end
